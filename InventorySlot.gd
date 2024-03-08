2115593b37043fe629275a7164fa32d1ff13b3ba
Ceisri <155374078+Ceisri@users.noreply.github.com>
2024-03-08 20:39:16 +0100

    Update InventorySlot.gd

--- InventorySlot.gd
+++ InventorySlot.gd
@@ -1,75 +1,96 @@
 extends TextureButton
 
-var DRAGPREVIEW = preload("res://Sprite.tscn")
+var DRAG_PREVIEW = preload("res://Sprite.tscn")
 onready var icon = $Icon
 onready var quantity_label = $Quantity
 
 var quantity = rand_range(0, 25)
+var item = "null"
 
 func _ready():
 	displayQuantity()
 	matchItemTypeToIcon()
-	
+
+func _physics_process(delta):
+	displayQuantity()
+
+
 func displayQuantity():
-	quantity_label.text = str(round(quantity))
+	if quantity != 0: 
+		quantity_label.text = str(round(quantity))
+	elif quantity == 0:
+		quantity_label.text = ""
+		icon.texture = preload("res://UI/graphics/SkillIcons/empty.png")
+		print("zeroed out")
+
 func get_drag_data(position: Vector2):
-		var slot = get_parent().get_name()
-		var data = {}
-		data["origin_node"] = self 
-		data["origin_slot"] = slot 
-		data["origin_texture_normal"] = icon.texture
-		data["quantity"] = quantity
-		data["item"] = item
-		var dragPreview = DRAGPREVIEW.instance()
-		dragPreview.texture = icon.texture
-		add_child(dragPreview)
-		
-		# Remove the icon from its original place
-		icon.texture = null
-		quantity_label.text = "" #this zero's out the quantity of the label when moving an item to another slot 
-		
-		return data
+	matchItemTypeToIcon()
+	
+	var slot = get_parent().get_name()
+	var data = {
+		"origin_node": self,
+		"origin_slot": slot,
+		"origin_texture": icon.texture,
+		"origin_quantity": quantity,
+		"origin_item": item
+	}
+	var dragPreview = DRAG_PREVIEW.instance()
+	dragPreview.texture = icon.texture
+	add_child(dragPreview)
+
+	# Remove the icon from its original place
+	icon.texture = preload("res://UI/graphics/SkillIcons/empty.png")
+	quantity = 0
+	displayQuantity()
+	print("Item type:", item)
+	return data
 
 func can_drop_data(position, data):
+	matchItemTypeToIcon()
+	displayQuantity()
 	var target_slot = get_parent().get_name()
-	return true 
+	data["target_texture"] = icon.texture
+	data["target_quantity"] = quantity
+	data["target_item"] = item
+	return true
+
 
 func drop_data(position, data):
-	icon.texture = data["origin_texture_normal"]
-	quantity = data["quantity"] #sets the quantity of the label based on the previous slot
-	checkIcon(data)
-	displayQuantity()
 	matchItemTypeToIcon()
-
-
-func checkIcon(data):
-	var receiving_quantity = data["quantity"]
-	var origin_slot_name = data["origin_slot"]
-
-
-	if item != data["item"]:
-		print(item + " " + data["item"])
-		# Set the quantity to the received quantity if the textures don't match
-		quantity = receiving_quantity
-	else:
-		# Increment the quantity if the textures match
-		quantity += receiving_quantity
-
 	displayQuantity()
+	var origin_texture = data["origin_texture"]
+	var target_texture = icon.texture
+	var origin_item = data["origin_item"]
+	var target_item = item
+	var origin_quantity = data["origin_quantity"]
+	var target_quantity = quantity
+
+	data["origin_node"].get_node("Icon").texture = target_texture
+	icon.texture = origin_texture
+
+	if origin_item == target_item:
+		# Only combine the quantities if target_texture and origin_texture are the same
+		quantity += data["origin_quantity"]
+		
+		data["origin_node"].get_node("Icon").texture = preload("res://UI/graphics/SkillIcons/empty.png")
+	else:
+		# Swap the quantities if the textures are different
+		quantity = origin_quantity
+		data["origin_quantity"] = target_quantity
 
 
-var item = "null"
 
 func matchItemTypeToIcon():
-	var icon_texture = icon.texture
-
-	if icon_texture == preload("res://UI/graphics/mushrooms/PNG/background/1.png"):
-		item = "mushroom 1"
-	elif icon_texture == preload("res://UI/graphics/mushrooms/PNG/background/2.png"):
-		item = "mushroom 2"
+	var texture_to_item = {
+		preload("res://UI/graphics/mushrooms/PNG/background/1.png"): "mushroom 1",
+		preload("res://UI/graphics/mushrooms/PNG/background/2.png"): "mushroom 2",
+		preload("res://UI/graphics/SkillIcons/empty.png"): "empty"
+	}
+	
+	if icon.texture in texture_to_item:
+		item = texture_to_item[icon.texture]
+	elif icon.texture == null:
+		item = "empty"
 	else:
-		# Default item type if the texture doesn't match known types
 		item = "unknown"
 
-	# Optionally, you can print or use the item type here
-	print("Item type:", item)
