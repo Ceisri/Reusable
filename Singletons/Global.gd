600a1a67bfe3f1ae1cc4e88b8b165df2582b103a
Alex <155374078+Ceisri@users.noreply.github.com>
2024-06-02 01:07:41 +0200

    Add files via upload

--- Singletons/Global.gd
+++ Singletons/Global.gd
@@ -56,46 +56,7 @@ enum state_list{
 	skillB}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#________________________________________Icons and their data_______________________________________
 
 onready var drag_preview = preload("res://scripts/Components/Sprite.tscn")
 #food_____________________________________
@@ -109,26 +70,17 @@ onready var carrot = preload("res://Food Icons/Vegetables/carrot.png")
 onready var potato = preload("res://Food Icons/Vegetables/potato.png")
 onready var garlic = preload("res://Food Icons/Vegetables/garlic.png")
 onready var onion = preload("res://Food Icons/Vegetables/onion.png")
-
 #grain
 onready var corn = preload("res://Food Icons/Vegetables/corn.png")
-
-
 #greens 
 onready var cabbage = preload("res://Food Icons/Vegetables/cabbage.png")
 onready var bell_pepper = preload("res://Food Icons/Vegetables/bell pepper.png")
 onready var aubergine = preload("res://Food Icons/Vegetables/aubergine.png")
-
-
-
 #coocked meals
 onready var bread = preload("res://Food Icons/Cooked Meals/Bread/bread.png")
 
 
 
-
-#weapons
-onready var wood_sword =  preload("res://0.png")
 onready var bow =  preload("res://Equipment icons/bows/bow.png")
 
 
@@ -139,7 +91,8 @@ onready var red_potion = preload("res://Potions/Red potion.png")
 onready var empty_potion = preload("res://Potions/Empty potion.png")
 onready var water = preload("res://Potions/water.png")
 
-
+#equipment__________________________________________________________________________________________
+onready var wood_sword =  preload("res://0.png")
 onready var hat1 = preload("res://Equipment icons/hat1.png")
 onready var pad1 = preload("res://Equipment icons/shoulder1.png")
 onready var garment1 = preload("res://Equipment icons/garment1.png")
@@ -148,18 +101,66 @@ onready var shoe1 = preload("res://Equipment icons/shoe1.png")
 onready var glove1 = preload("res://Equipment icons/glove1.png")
 onready var belt1 = preload("res://Equipment icons/belt1.png")
 
-
 onready var torso_armor2 = preload("res://Equipment icons/torso2.png")
 onready var torso_armor3 = preload("res://Equipment icons/torso3.png")
 onready var torso_armor4 = preload("res://Equipment icons/torso4.png")
 
 onready var staff1 = preload("res://Equipment icons/staves/staff1.png")
-
-
 onready var shoulder1 = preload("res://Equipment icons/shouleder pads/shoulder plate metal.png")
 
+#_________________________________Skill icons___________________________________
 
-#_______________________________3D equipable items______________________________
+#necromant
+onready var necromant_switch =  preload("res://Classes/Ability Icons/Magic Icons1/41.png")
+onready var base_attack_necromant =  preload("res://Classes/Ability Icons/Magic Icons1/1.png")
+onready var necro_guard =  preload("res://Classes/Ability Icons/Magic Icons1/14.png")
+onready var dominion =  preload("res://Classes/Necromant/Class Icons/Dominion.png")
+onready var summon_shadow =  preload("res://Classes/Necromant/Class Icons/SummonShadow.png")
+onready var tribute =  preload("res://Classes/Necromant/Class Icons/Tribute.png")
+onready var servitude =  preload("res://Classes/Necromant/Class Icons/Servitude in Death.png")
+onready var sacrifice =  preload("res://Classes/Necromant/Class Icons/Sacrifice.png")
+onready var arcane_blast =  preload("res://Classes/Ability Icons/Magic Icons1/5.png")
+
+
+#base attacks
+onready var punch =  preload("res://UI/graphics/SkillIcons/fist.png")
+onready var guard =  preload("res://Classes/handcombat/guard.png")
+#sword______________________________________________________________________________________________
+onready var slash_sword =  preload("res://Classes/Swordsmen/slash.png")
+onready var guard_sword =  preload("res://Classes/Swordsmen/cross parry.png")
+#bow________________________________________________________________________________________________
+onready var quick_shot =  preload("res://Classes/archery icons/quick_shot.png")
+var quick_shot_damage: float = 2.5
+onready var full_draw =  preload("res://Classes/archery icons/full_draw.png")
+var full_draw_damage: float = 5
+
+#sword_skills
+onready var overhead_slash =  preload("res://Classes/Swordsmen/overhead slash.png")
+#___________________________________________________________________________________________________
+onready var rising_slash =  preload("res://Classes/Swordsmen/flury of blows.png")
+var rising_slash_damage: float = 3
+#___________________________________________________________________________________________________
+onready var counter_strike =  preload("res://Classes/Swordsmen/counter strike.png")
+var counter_strike_damge:float  = 7.5
+var counter_strike_cost:float  = 5
+#___________________________________________________________________________________________________
+onready var cyclone =  preload("res://Classes/Swordsmen/cyclone.png")
+var cyclone_damage: float = 7
+var cyclone_cooldown: float = 12
+var cyclone_cost: float = 21
+var cyclone_description: String = "Base damage: 3 hits of 7 SLASH DAMAGE                        Cost: 15 resolve                                                       Cooldown: 12 seconds"
+var cyclone_description2: String = "+5% compounding extra damage per skill level                +SLASH DAMAGE                                                               'spin and slash foes around you in an area attack, each foe can be hit up to 3 times"
+#___________________________________________________________________________________________________
+onready var fury_strike =  preload("res://Classes/Swordsmen/fury strike.png")
+var base_fury_strike_damage: float = 5
+#___________________________________________________________________________________________________
+onready var rising_fury =  preload("res://Classes/Swordsmen/scream.png")
+var rising_fury_threat: float = 25
+
+
+
+
+#______________________________________3D equipable items___________________________________________
 onready var shoulder_scene0: PackedScene = preload("res://Equipment/Shoulder pads/shoulder plate metal.glb")
 
 
@@ -197,70 +198,6 @@ func addFloatingIcon(parent,texture,quantity):
 	instance.amount = quantity
 	parent.add_child(instance)
 
-
-
-#_________________________________Skill icons___________________________________
-
-#necromant
-
-onready var necromant_switch =  preload("res://Classes/Ability Icons/Magic Icons1/41.png")
-onready var base_attack_necromant =  preload("res://Classes/Ability Icons/Magic Icons1/1.png")
-onready var necro_guard =  preload("res://Classes/Ability Icons/Magic Icons1/14.png")
-
-
-
-onready var dominion =  preload("res://Classes/Necromant/Class Icons/Dominion.png")
-onready var summon_shadow =  preload("res://Classes/Necromant/Class Icons/SummonShadow.png")
-onready var tribute =  preload("res://Classes/Necromant/Class Icons/Tribute.png")
-onready var servitude =  preload("res://Classes/Necromant/Class Icons/Servitude in Death.png")
-onready var sacrifice =  preload("res://Classes/Necromant/Class Icons/Sacrifice.png")
-onready var arcane_blast =  preload("res://Classes/Ability Icons/Magic Icons1/5.png")
-
-
-
-
-
-
-#base skills
-onready var punch =  preload("res://UI/graphics/SkillIcons/fist.png")
-onready var guard =  preload("res://Classes/handcombat/guard.png")
-
-#sword_skills
-onready var slash_sword =  preload("res://Classes/Swordsmen/slash.png")
-onready var guard_sword =  preload("res://Classes/Swordsmen/cross parry.png")
-onready var overhead_slash =  preload("res://Classes/Swordsmen/overhead slash.png")
-#___________________________________________________________________________________________________
-onready var rising_slash =  preload("res://Classes/Swordsmen/flury of blows.png")
-var rising_slash_damage: float = 3
-#___________________________________________________________________________________________________
-onready var counter_strike =  preload("res://Classes/Swordsmen/counter strike.png")
-var counter_strike_damge:float  = 7.5
-var counter_strike_cost:float  = 5
-#___________________________________________________________________________________________________
-onready var cyclone =  preload("res://Classes/Swordsmen/cyclone.png")
-var cyclone_damage: float = 7
-#___________________________________________________________________________________________________
-onready var fury_strike =  preload("res://Classes/Swordsmen/fury strike.png")
-var base_fury_strike_damage: float = 5
-#___________________________________________________________________________________________________
-onready var rising_fury =  preload("res://Classes/Swordsmen/scream.png")
-var rising_fury_threat: float = 25
-
-
-
-
-
-
-
-
-
-
-
-#bow_skills
-onready var quick_shot =  preload("res://Classes/archery icons/quick_shot.png")
-var quick_shot_damage: float = 2.5
-onready var full_draw =  preload("res://Classes/archery icons/full_draw.png")
-var full_draw_damage: float = 5
 
 
 
