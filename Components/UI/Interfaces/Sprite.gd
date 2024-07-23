a62cb2f2c4729e9abb5709e4171fc2b2d2ce7dd9
Alex <155374078+Ceisri@users.noreply.github.com>
2024-07-23 12:40:40 +0200

    Delete Components directory

--- Components/UI/Interfaces/Sprite.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite
-
-
-func _physics_process(_delta)->void:
-	if Engine.get_physics_frames() % 2 == 0:
-		global_position = get_global_mouse_position()
-		if Input.is_action_just_released("ESC") or Input.is_action_just_released("Inventory") or Input.is_action_just_released("skills"):
-			queue_free()
-			
