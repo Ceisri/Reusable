37a0143c27deb9512e4a09bc8262909bb7d8bbc8
Alex <155374078+Ceisri@users.noreply.github.com>
2024-07-23 12:37:23 +0200

    Delete Items.gd

--- Items.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-
-var empty_potion = load("res://Game/Interface/Assets/Icons/Alchemy/Alchemy_09_test_tube.png")
-var red_potion = load("res://Game/Interface/Assets/Icons/Alchemy/Alchemy_13_heal_potion.png")
-
-
-var blue_tip_grass = load("res://Game/Interface/Assets/Icons/Herbalism/Herbalism_15_herb.png")
-var blue_tip_grass_rarity = 12
